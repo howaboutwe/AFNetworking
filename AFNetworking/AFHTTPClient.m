99696d4c778600e385bac3d879e79169f5c15ae2
Jerry Wong <jerry@howaboutwe.com>
2012-08-03 15:35:42 -0400

    Made operations return on URL request.

--- AFNetworking/AFHTTPClient.m
+++ AFNetworking/AFHTTPClient.m
@@ -611,7 +611,7 @@ static void AFNetworkReachabilityReleaseCallback(const void *info) {
 
 #pragma mark -
 
-- (void)getPath:(NSString *)path 
+- (AFHTTPRequestOperation *)getPath:(NSString *)path
      parameters:(NSDictionary *)parameters 
         success:(void (^)(AFHTTPRequestOperation *operation, id responseObject))success
         failure:(void (^)(AFHTTPRequestOperation *operation, NSError *error))failure
@@ -619,9 +619,10 @@ static void AFNetworkReachabilityReleaseCallback(const void *info) {
 	NSURLRequest *request = [self requestWithMethod:@"GET" path:path parameters:parameters];
     AFHTTPRequestOperation *operation = [self HTTPRequestOperationWithRequest:request success:success failure:failure];
     [self enqueueHTTPRequestOperation:operation];
+    return operation;
 }
 
-- (void)postPath:(NSString *)path 
+- (AFHTTPRequestOperation *)postPath:(NSString *)path
       parameters:(NSDictionary *)parameters 
          success:(void (^)(AFHTTPRequestOperation *operation, id responseObject))success
          failure:(void (^)(AFHTTPRequestOperation *operation, NSError *error))failure
@@ -629,9 +630,10 @@ static void AFNetworkReachabilityReleaseCallback(const void *info) {
 	NSURLRequest *request = [self requestWithMethod:@"POST" path:path parameters:parameters];
 	AFHTTPRequestOperation *operation = [self HTTPRequestOperationWithRequest:request success:success failure:failure];
     [self enqueueHTTPRequestOperation:operation];
+    return operation;
 }
 
-- (void)putPath:(NSString *)path 
+- (AFHTTPRequestOperation *)putPath:(NSString *)path
      parameters:(NSDictionary *)parameters 
         success:(void (^)(AFHTTPRequestOperation *operation, id responseObject))success
         failure:(void (^)(AFHTTPRequestOperation *operation, NSError *error))failure
@@ -639,9 +641,10 @@ static void AFNetworkReachabilityReleaseCallback(const void *info) {
 	NSURLRequest *request = [self requestWithMethod:@"PUT" path:path parameters:parameters];
 	AFHTTPRequestOperation *operation = [self HTTPRequestOperationWithRequest:request success:success failure:failure];
     [self enqueueHTTPRequestOperation:operation];
+    return operation;
 }
 
-- (void)deletePath:(NSString *)path 
+- (AFHTTPRequestOperation *)deletePath:(NSString *)path
         parameters:(NSDictionary *)parameters 
            success:(void (^)(AFHTTPRequestOperation *operation, id responseObject))success
            failure:(void (^)(AFHTTPRequestOperation *operation, NSError *error))failure
@@ -649,9 +652,10 @@ static void AFNetworkReachabilityReleaseCallback(const void *info) {
 	NSURLRequest *request = [self requestWithMethod:@"DELETE" path:path parameters:parameters];
 	AFHTTPRequestOperation *operation = [self HTTPRequestOperationWithRequest:request success:success failure:failure];
     [self enqueueHTTPRequestOperation:operation];
+    return operation;
 }
 
-- (void)patchPath:(NSString *)path 
+- (AFHTTPRequestOperation *)patchPath:(NSString *)path
        parameters:(NSDictionary *)parameters 
           success:(void (^)(AFHTTPRequestOperation *operation, id responseObject))success
           failure:(void (^)(AFHTTPRequestOperation *operation, NSError *error))failure
@@ -659,6 +663,7 @@ static void AFNetworkReachabilityReleaseCallback(const void *info) {
     NSURLRequest *request = [self requestWithMethod:@"PATCH" path:path parameters:parameters];
 	AFHTTPRequestOperation *operation = [self HTTPRequestOperationWithRequest:request success:success failure:failure];
     [self enqueueHTTPRequestOperation:operation];
+    return operation;
 }
 
 @end
